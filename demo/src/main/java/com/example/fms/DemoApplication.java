f362db9f2a59fb9d4d7a7c2de3e278c0d68a1561
alanmohan <alanmohan86@gmail.com>
2022-12-05 22:10:51 +0400

    added new part

--- demo/src/main/java/com/example/fms/DemoApplication.java
+++ demo/src/main/java/com/example/fms/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.fms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ demo/src/main/java/com/example/fms/UserController.java
@@ -0,0 +1,18 @@
+package com.example.fms;
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+@Controller
+public class UserController {
+    @GetMapping("/FlightInfo")
+    public String StartFlightInfo(Model model) {
+        FlightInfoUser user = new FlightInfoUser();
+         model.addAttribute("user", user);
+         
+        return "FlightInfoUser";
+    }
+    @PostMapping(path="/FlightInfo")
+    public String EndFlightInfo(@ModelAttribute("user") FlightInfoUser user) {
+        return "ShowAvailableFlight";
+    }
+}
