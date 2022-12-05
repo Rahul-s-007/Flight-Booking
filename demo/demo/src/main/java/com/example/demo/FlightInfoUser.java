2df487708c8f10da5bcb06b9d9bef056ee6c69a9
Rahul-s-007 <70894220+Rahul-s-007@users.noreply.github.com>
2022-12-05 18:30:48 +0400

    initial files for spring boot

---
+++ demo/demo/src/main/java/com/example/demo/FlightInfoUser.java
@@ -0,0 +1,42 @@
+package com.example.demo;
+import java.sql.Date;
+
+public class FlightInfoUser {
+    private String from;
+    private String to;
+    private Date date;
+    private int passengers;
+    public void setFrom(String from)
+    {
+        this.from=from;
+    }
+    public void setTo(String to)
+    {
+        this.to=to;
+    }
+    public void setDate(Date date)
+    {
+        this.date=date;
+    }
+    public void setPassengers(int passengers)
+    {
+        this.passengers=passengers;
+    }
+    public String getFrom()
+    {
+        return from;
+    }
+    public String getTo()
+    {
+        return to;
+    }
+    public Date getDate()
+    {
+        return date;
+    }
+    public int getPassengers()
+    {
+        return passengers;
+    }
+}
+
