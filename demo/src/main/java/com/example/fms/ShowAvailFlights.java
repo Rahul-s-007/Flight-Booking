dbefc82f3b720f31eee56829f4c72ab8514912a0
alanmohan <alanmohan86@gmail.com>
2022-12-06 09:34:58 +0400

    new changes

---
+++ demo/src/main/java/com/example/fms/ShowAvailFlights.java
@@ -0,0 +1,13 @@
+package com.example.fms;
+public class ShowAvailFlights
+{
+    private String FlightNo;
+    public void setFlightNo(String FlightNo)
+    {
+        this.FlightNo=FlightNo;
+    }
+    public String getFlightNo()
+    {
+        return FlightNo;
+    }
+}
--- demo/src/main/java/com/example/fms/UserController.java
+++ demo/src/main/java/com/example/fms/UserController.java
@@ -1,18 +1,50 @@
 package com.example.fms;
 import org.springframework.ui.Model;
+
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 @Controller
 public class UserController {
     @GetMapping("/FlightInfo")
     public String StartFlightInfo(Model model) {
-        FlightInfoUser user = new FlightInfoUser();
-         model.addAttribute("user", user);
-         
-        return "FlightInfoUser";
+        FlightInfoUser Flightinfo = new FlightInfoUser();
+        model.addAttribute("Flightinfo", Flightinfo);
+        return "FlightInfo";
     }
     @PostMapping(path="/FlightInfo")
-    public String EndFlightInfo(@ModelAttribute("user") FlightInfoUser user) {
+    public String EndFlightInfo(@ModelAttribute("Flightinfo") FlightInfoUser Flightinfo) {
         return "ShowAvailableFlight";
+    }
+    @GetMapping("/ShowAvailableFlight")
+    public String StartShowFlights(Model model)
+    {
+        ShowAvailFlights FlightNoSelected = new ShowAvailFlights();
+        model.addAttribute("FlightNoSelected", FlightNoSelected);
+        ArrayList<String> FlightNo=new ArrayList<String>();
+        ArrayList<String> Departure=new ArrayList<String>();
+        ArrayList<String> Arrival=new ArrayList<String>();
+        ArrayList<Integer> Economy=new ArrayList<Integer>();
+        ArrayList<Integer> Business=new ArrayList<Integer>();
+        //here we call the query for the flight available 
+        model.addAttribute("FlightNo", FlightNo);
+        model.addAttribute("Departure", Departure);
+        model.addAttribute("Arrival", Arrival);
+        model.addAttribute("Economy", Economy);
+        model.addAttribute("Business", Business);
+        return "ShowAvailableFlight";
+    }
+    @PostMapping("/ShowAvailableFlight")
+    public String EndShowFlights(@ModelAttribute("FlightNo") ShowAvailFlights FlightNoSelected)
+    {
+        return "AvailableSeats";
+    }
+    @GetMapping("/AvailableSeats")
+    public String StartAvailableSeats(Model model)
+    {
+        ArrayList<String> FilledSeats=new ArrayList<String>();
+        model.addAttribute("FilledSeats", FilledSeats);
+        return "AvailableSeats";
     }
 }
